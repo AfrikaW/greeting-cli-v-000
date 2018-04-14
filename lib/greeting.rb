359a3a882bc7ed56beff69374f65669a7606ae42
Afrika Williams <afrika.williams@gmail.com>
2018-04-14 20:10:29 +0000

    automatically backed up by learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,11 @@
 # code the #greeting method here!
+puts "'Hi! I'm HAL, what's your name?"
+
+def greeting(name)
+  puts "Hello #{name}. It's nice to meet you."
+end
+
+greeting(name) 
+
+gets.strip = name
+  
